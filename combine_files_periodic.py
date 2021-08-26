bf95150b9a33a2fd6586a3301a7b0b027fe0db4d
Oliver H. Philcox <ophilcox@della5.princeton.edu>
2021-08-26 15:15:16 -0400

    general updates

---
+++ combine_files_periodic.py
@@ -0,0 +1,140 @@
+### combine_files_periodic.py (Oliver Philcox, 2021)
+# This reads in a set of (data-random) particle counts and uses them to construct the N-point functions, assuming a periodic geometry
+# It is designed to be used with the run_npcf.csh script
+# Currently 2PCF, 3PCF, 4PCF, 5PCF and 6PCF are supported, and the R^N random counts are computed analytically.
+# The output is saved to the working directory with the same format as the NPCF counts, with the filename ...zeta_{N}pcf.txt
+
+import sys, os
+import numpy as np
+
+## First read-in the input file string from the command line
+if len(sys.argv)!=6:
+    raise Exception("Need to specify the input files, number of galaxies, boxsize, R_min and R_max!")
+else:
+    inputs = str(sys.argv[1])
+    N_gal = int(sys.argv[2])
+    boxsize = np.float64(sys.argv[3])
+    R_min = np.float64(sys.argv[4])
+    R_max = np.float64(sys.argv[5])
+n_bar = np.float64(N_gal)/boxsize**3.
+
+print("Reading in files starting with %s\n"%inputs)
+
+# Decide which N we're using
+Ns = []
+for N in range(10):
+    R_file = inputs+'.r_%dpcf.txt'%N
+    if os.path.exists(R_file):
+        Ns.append(N)
+
+for N in Ns:
+    # Load in first D-R piece to get bins
+    tmp_file = inputs+'.n00_%dpcf.txt'%(N)
+    if N==2:
+        counts_tmp = np.loadtxt(tmp_file,skiprows=5) # skipping rows with radial bins
+    else:
+        counts_tmp = np.loadtxt(tmp_file,skiprows=5+N) # skipping rows with radial bins
+
+    # Extract ells and radial bins
+    if N==2:
+        bin1 = np.loadtxt(tmp_file,skiprows=4,max_rows=1)
+    elif N==3:
+        ell_1 = np.asarray(counts_tmp[:,0],dtype=int)
+        max_ell = np.max(ell_1)
+        bin1,bin2 = np.loadtxt(tmp_file,skiprows=6,max_rows=2)
+    elif N==4:
+        ell_1,ell_2,ell_3 = np.asarray(counts_tmp[:,:3],dtype=int).T
+        max_ell = np.max(ell_1)
+        bin1,bin2,bin3 = np.loadtxt(tmp_file,skiprows=6,max_rows=3)
+    elif N==5:
+        ell_1,ell_2,ell_12,ell_3,ell_4 = np.asarray(counts_tmp[:,:5],dtype=int).T
+        max_ell = np.max(ell_1)
+        bin1,bin2,bin3,bin4 = np.loadtxt(tmp_file,skiprows=6,max_rows=4)
+    elif N==6:
+        ell_1,ell_2,ell_12,ell_3,ell_123,ell_4,ell_5 = np.asarray(counts_tmp[:,:7],dtype=int).T
+        max_ell = np.max(ell_1)
+        bin1,bin2,bin3,bin4,bin5 = np.loadtxt(tmp_file,skiprows=6,max_rows=5)
+    else:
+        raise Exception("%dPCF not yet configured"%N)
+
+    # Now load in D-R pieces and average
+    countsN_all = []
+    total_DmR = 0
+    for i in range(100):
+        DmR_file = inputs+'.n%s_%dpcf.txt'%(str(i).zfill(2),N)
+        if not os.path.exists(DmR_file): continue
+        # Extract counts
+        if N==2:
+            countsN_all.append(np.loadtxt(DmR_file,skiprows=5))
+        if N==3:
+            countsN_all.append(np.loadtxt(DmR_file,skiprows=8)[:,1:]) # skipping rows with radial bins and ell
+        elif N==4:
+            countsN_all.append(np.loadtxt(DmR_file,skiprows=9)[:,3:])
+        elif N==5:
+            countsN_all.append(np.loadtxt(DmR_file,skiprows=10)[:,5:])
+        elif N==6:
+            countsN_all.append(np.loadtxt(DmR_file,skiprows=11)[:,7:])
+    countsN_all = np.asarray(countsN_all)
+    N_files = len(countsN_all)
+    countsN = np.mean(countsN_all,axis=0)
+    # could use this next line to compute std from finite number of randoms!
+    #countsNsig = np.std(countsN_all,axis=0)/np.sqrt(N_files)
+
+    # Function to compute radius from bin index
+    radius = lambda bin, n_r: R_min + 1.*bin*(R_max-R_min)/n_r
+    # Function to compute bin volume * n-bar from bin index
+    nV = lambda bin, n_r: n_bar*4.*np.pi/3.*(radius(bin+1,n_r)**3.-radius(bin,n_r)**3.)
+
+    ### Now compute analytic randoms
+    ### For N>2 we include the basis function P_{0} = (4\pi)^{-(N-1)/2}
+    if N==2:
+        n_r = 1.*len(bin1)
+        R_analyt = N_gal*nV(bin1,n_r)
+    elif N==3:
+        n_r = len(np.unique(np.concatenate([bin1,bin2])))
+        R_analyt = N_gal*nV(bin1,n_r)*nV(bin2,n_r)/(4.*np.pi)
+    elif N==4:
+        n_r = len(np.unique(np.concatenate([bin1,bin2,bin3])))
+        R_analyt = N_gal*nV(bin1,n_r)*nV(bin2,n_r)*nV(bin3,n_r)/(4.*np.pi)**1.5
+    elif N==5:
+        n_r = len(np.unique(np.concatenate([bin1,bin2,bin3,bin4])))
+        R_analyt = N_gal*nV(bin1,n_r)*nV(bin2,n_r)*nV(bin3,n_r)*nV(bin4,n_r)/(4.*np.pi)**2.
+    elif N==6:
+        n_r = len(np.unique(np.concatenate([bin1,bin2,bin3,bin4,bin5])))
+        R_analyt = N_gal*nV(bin1,n_r)*nV(bin2,n_r)*nV(bin3,n_r)*nV(bin4,n_r)*nV(bin5,n_r)/(4.*np.pi)**2.5
+
+    # Now compute full NPCF
+    # We do not apply any edge-correction, such that zeta = (D-R)_Lambda / R_{Lambda=0} * (4 pi)^{(N-1)/2}
+    # i.e. we assume R_{Lambda} = 0 for Lambda > (00...0)
+    # the (4 pi) factor comes from the P_{0} basis function.
+
+    if N==2:
+        # no (4 pi) factors here since no angular basis functions!
+        zeta = countsN/R_analyt
+    else:
+        zeta = np.zeros_like(countsN)
+        for i in range(len(bin1)):
+            zeta[:,i] = countsN[:,i]/R_analyt[i]*(4.*np.pi)**(0.5*(N-1.))
+
+    # Now save the output to file, copying the first few lines from the N files
+    zeta_file = inputs+'.zeta_%dpcf.txt'%N
+    rfile = open(tmp_file,"r")
+    zfile = open(zeta_file,"w")
+    if N==2:
+        for l,line in enumerate(rfile):
+            if l>=5: continue
+            zfile.write(line)
+        for a in range(len(zeta)):
+            zfile.write("%.8e\t"%zeta[a])
+    else:
+        for l,line in enumerate(rfile):
+            if l>=5+N: continue
+            zfile.write(line)
+        for a in range(len(zeta)):
+            zfile.write("%d\t"%ell_1[a])
+            for b in range(len(zeta[a])):
+                zfile.write("%.8e\t"%zeta[a,b])
+            zfile.write("\n")
+    zfile.close()
+
+    print("Computed periodic %dPCF using %d (data-random) files, saving to %s\n"%(N,N_files,zeta_file))
---
+++ coupling_weights.py
@@ -0,0 +1,139 @@
+### coupling_weights.py (Oliver Philcox, 2021)
+# Compute the coupling coefficients C_m^Lambda for the NPCF basis functions
+# These are saved in a .txt file as a one-dimensional array and will be read in by the C++ code.
+# Input parameters are N and LMAX (with 3<=N<=6).
+# We caution that the computation time scales as (LMAX+1)^{3N-7}, so large LMAX should be avoided for high N.
+# Note that we save only the parity-even coefficients (with even l1+l2+...+lN)
+# Note also that internal momenta, e.g. l12, l123 (for N>4) can extend to 2*LMAX by the 3j conditions.
+
+import sys, os
+import numpy as np
+from sympy.physics.wigner import wigner_3j
+
+## First read-in the input parameters from the command line
+if len(sys.argv)!=3:
+    raise Exception("Need to specify N and LMAX")
+else:
+    N = int(sys.argv[1])
+    LMAX = int(sys.argv[2])
+
+print("\nComputing the coupling coefficients for the %dPCF up to l_max = %d\n"%(N,LMAX))
+
+if N==3:
+    # 3PCF computation
+    def WeightFunction(l,m):
+        """C_m^Lambda for 2nd order isotropic basis function"""
+        return pow(-1.,l1-m1)/np.sqrt(2.*l1+1.)
+
+    WeightsNPCF = np.zeros(int((LMAX+1)*(LMAX+2)/2.))
+    for l1 in range(LMAX+1):
+        for m1 in range(l1+1):
+            weight = WeightFunction(l1,m1)
+            if weight!=0:
+                WeightsNPCF[l1*(l1+1)//2+m1] = weight
+
+if N==4:
+    # 4PCF computation
+    def WeightFunction(l1,l2,l3,m1,m2,m3):
+        """C_m^Lambda for 3rd order isotropic basis function"""
+        return wigner_3j(l1,l2,l3,m1,m2,m3)*pow(-1.,l1+l2+l3)
+
+    # Create 1D output matrix. The size is an overestimate and will be trimmed down later.
+    WeightsNPCF = np.zeros(((LMAX+1)**5))
+    index = 0
+    for l1 in range(LMAX+1):
+        for l2 in range(LMAX+1):
+            for l3 in range(abs(l1-l2),min(LMAX,l1+l2)+1):
+                if pow(-1.,l1+l2+l3)==-1: continue # odd parity
+                for m1 in range(-l1,l1+1):
+                    for m2 in range(-l2,l2+1):
+                        m3 = -m1-m2
+                        if abs(m3)>l3: continue
+                        weight = WeightFunction(l1,l2,l3,m1,m2,m3)
+                        if weight!=0:
+                            WeightsNPCF[index] = weight
+                        index += 1
+
+    # trim down to remove trivial elements
+    WeightsNPCF = WeightsNPCF[:index]
+
+if N==5:
+    # 5PCF computation
+    def WeightFunction(l1,l2,l12,l3,l4,m1,m2,m3,m4):
+        """C_m^Lambda for 4th order isotropic basis function"""
+        pref = pow(-1.,l1+l2+l3+l4)*np.sqrt(2.*l12+1.)
+        wig1 = np.float64(wigner_3j(l1,l2,l12,m1,m2,-m1-m2))
+        if wig1==0: return 0.
+        wig2 = np.float64(wigner_3j(l12,l3,l4,m1+m2,m3,m4))
+        if wig2==0: return 0.
+        summ = wig1*wig2*pow(-1.,l12-m1-m2)
+        return summ*pref
+
+    # Create 1D output matrix. The size is an overestimate and will be trimmed down later.
+    WeightsNPCF = np.zeros(((LMAX+1)**7)*(2*LMAX+1))
+    index = 0
+    for l1 in range(LMAX+1):
+        for l2 in range(LMAX+1):
+            for l12 in range(abs(l1-l2),l1+l2+1):
+                for l3 in range(LMAX+1):
+                    for l4 in range(abs(l12-l3),min(LMAX,l12+l3)+1):
+                        if pow(-1.,l1+l2+l3+l4)==-1: continue # odd parity
+                        for m1 in range(-l1,l1+1):
+                            for m2 in range(-l2,l2+1):
+                                if abs(m1+m2)>l12: continue # m12 condition
+                                for m3 in range(-l3,l3+1):
+                                    m4 = -m1-m2-m3
+                                    if abs(m4)>l4: continue
+                                    weight = WeightFunction(l1,l2,l12,l3,l4,m1,m2,m3,m4)
+                                    if weight!=0:
+                                        WeightsNPCF[index] = weight
+                                    index += 1
+
+    # trim down to remove trivial elements
+    WeightsNPCF = WeightsNPCF[:index]
+
+if N==6:
+    # 6PCF computation
+    def WeightFunction(l1,l2,l12,l3,l123,l4,l5,m1,m2,m3,m4,m5):
+        """C_m^Lambda for 5th order isotropic basis function"""
+        pref = pow(-1.,l1+l2+l3+l4+l5)*np.sqrt(2.*l12+1.)*np.sqrt(2.*l123+1.)
+        # noting m12 = m1+m2, m123 = m1+m2+m3
+        wig1 = wigner_3j(l1,l2,l12,m1,m2,-m1-m2)
+        if wig1==0: return 0.
+        wig2 = wigner_3j(l12,l3,l123,m1+m2,m3,-m1-m2-m3)
+        if wig2==0: return 0.
+        wig3 = wigner_3j(l123,l4,l5,m1+m2+m3,m4,m5)
+        if wig3==0: return 0.
+        summ = wig1*wig2*wig3*pow(-1.,l12-m1-m2+l123-m1-m2-m3)
+        return summ*pref
+
+    # Create 1D output matrix. The size is an overestimate and will be trimmed down later.
+    WeightsNPCF = np.zeros(((LMAX+1)**9*(2*LMAX+1)**2))
+    index = 0
+    for l1 in range(LMAX+1):
+        for l2 in range(LMAX+1):
+            for l12 in range(abs(l1-l2),l1+l2+1):
+                for l3 in range(LMAX+1):
+                    for l123 in range(abs(l12-l3),l12+l3+1):
+                        for l4 in range(LMAX+1):
+                            for l5 in range(abs(l123-l4),min(LMAX,l123+l4)+1):
+                                if pow(-1.,l1+l2+l3+l4+l5)==-1: continue # odd parity
+                                for m1 in range(-l1,l1+1):
+                                    for m2 in range(-l2,l2+1):
+                                        if abs(m1+m2)>l12: continue # m12 condition
+                                        for m3 in range(-l3,l3+1):
+                                            if abs(m1+m2+m3)>l123: continue # m123 condition
+                                            for m4 in range(-l4,l4+1):
+                                                m5 = -m1-m2-m3-m4
+                                                if abs(m5)>l5: continue
+                                                weight = WeightFunction(l1,l2,l12,l3,l123,l4,l5,m1,m2,m3,m4,m5)
+                                                if weight!=0:
+                                                    WeightsNPCF[index] = weight
+                                                index += 1
+
+    # trim down to remove trivial elements
+    WeightsNPCF = WeightsNPCF[:index]
+
+outfile = 'coupling_matrices/weights_%dpcf_LMAX%d.txt'%(N,LMAX)
+np.savetxt(outfile,WeightsNPCF)
+print("Coefficients saved to %s"%outfile)
